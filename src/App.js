3cf39cbbd8eb3c458f73904639349310e22ac1ac
Dency-28 <angelmist2872@gmail.com>
2025-09-29 21:23:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div>
+      <h1>Hello from js-frontend!</h1>
+    </div>
+  );
+}
+
+export default App;
